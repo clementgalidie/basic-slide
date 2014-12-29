db5fc54301227001115814d11157533fe684257c
Clement Galidie <galidie.clement@gmail.com>
2014-12-29 23:58:21 +0100

    Update slider.js and index.html files

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,3 +1,11 @@
 (function () {
-	"use strict";
+	"use strict"; // IEF + strict mode.
+
+	// Get all the slides.
+	var slides = document.querySelectorAll(".slider-items");
+	
+	// Get the previous button.
+	var prevButton = document.getElementById("previous");
+	// Get the next button.
+	var nextButton = document.getElementById("next");
 }());
