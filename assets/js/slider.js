4fb4daab75bdb18eef87b06efcc7020386f6e262
Clément Galidie <galidie.clement@gmail.com>
2014-12-30 02:08:58 +0100

    Update prev and next buttons + create next function in slider.js file

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -8,4 +8,20 @@
 	var prevButton = document.getElementById("previous");
 	// Get the next button.
 	var nextButton = document.getElementById("next");
+
+	// Get previous slide.
+	var previousSlide = function () {
+
+	}
+
+	// Get next slide.
+	var nextSlide = function () {
+		var visibleSlide = document.querySelector(".slider-items--visible");
+		var nextVisibleSlide = visibleSlide.nextSibling.nextSibling;
+		nextVisibleSlide.classList.add("slider-items--visible");
+		visibleSlide.classList.remove("slider-items--visible");
+	}
+
+	nextButton.addEventListener("click", nextSlide);
+
 }());
